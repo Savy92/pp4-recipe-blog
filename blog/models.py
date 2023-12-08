eeef6d40da44888d52b19a5520bff45483e30f57
Savy92 <>
2023-12-08 21:07:50 +0000

    create models for UserProfile, Recipe, Comments

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=30, blank=True)
+    last_name = models.CharField(max_length=30, blank=True)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200, null=False, blank=False)
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="recipes")
+    featured_image = CloudinaryField("image", default="placeholder")
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    ingredients = models.TextField()
+    estimated_time = models.IntegerField("Estimated time")
+    likes = models.ManyToManyField(
+        User, related_name='liked_recipes', blank=True)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
+
+    def number_of_likes(self):
+        return self.likes.count()
+
+
+class Comment(models.Model):
+    recipe = models.ForeignKey(
+        Recipe, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f'{self.author.username} on {self.content}'
