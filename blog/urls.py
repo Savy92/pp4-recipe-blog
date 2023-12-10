1060836261981c72b5acbe3b27af801a8853d929
Savy92 <>
2023-12-10 18:54:37 +0000

    add RecipeDetails to views.py and create template for recipe_details

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.RecipeList.as_view(), name="home"),
+    path('recipe/<int:pk>/', views.RecipeDetails.as_view(), name='recipe_detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views import generic
+from django.views import generic, View
 from .models import Recipe
+from .forms import CommentForm
 
 
 class RecipeList(generic.ListView):
@@ -8,3 +9,47 @@ class RecipeList(generic.ListView):
     queryset = Recipe.objects.order_by("-created_on")
     template_name = "index.html"
     paginate_by = 6
+
+
+class RecipeDetails(View):
+
+    def get(self, request, pk, *args, **kwargs):
+        recipe = get_object_or_404(Recipe, pk=pk)
+        comments = recipe.comments.filter(
+            approved=True).order_by("-created_on")
+        comment_form = CommentForm()
+        liked = False
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "recipe_detail.html",
+            {
+                "recipe": recipe,
+                "comments": comments,
+                "comment_form": comment_form,
+                "liked": liked
+            },
+        )
+
+    def post(self, request, pk, *args, **kwargs):
+        recipe = get_object_or_404(Recipe, pk=pk)
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.recipe = recipe
+            new_comment.author = request.user
+            new_comment.save()
+            return redirect('recipe_detail', pk=pk)
+        comments = recipe.comments.filter(approved=True)
+
+        return render(
+            request,
+            "recipe_detail.html",
+            {
+                "recipe": recipe,
+                "comments": comments,
+                "comment_form": comment_form
+            },
+        )
