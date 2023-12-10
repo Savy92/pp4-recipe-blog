691e786a6779419a86461c9e721d7a27e7bfaf4b
Savy92 <>
2023-12-10 12:53:40 +0000

    add first view for RecipeList & create basic template for index.html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import generic
+from .models import Recipe
 
-# Create your views here.
+
+class RecipeList(generic.ListView):
+    model = Recipe
+    queryset = Recipe.objects.order_by("-created_on")
+    template_name = "index.html"
+    paginate_by = 6
